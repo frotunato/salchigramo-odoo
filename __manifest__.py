4d8d7201fcaa2c77e3b5376a5fe194115cf2ba38
frotunato <frotunato@gmail.com>
2020-05-01 22:01:35 +0200

    final commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-'name': "00-Salchigramo",
+'name': "Salchigramo",
 
 'summary': """
-Short (1 phrase/line) summary of the module's purpose, used as
-subtitle on modules listing or apps.openerp.com""",
-
-'description': """
-Long description of module's purpose
+    Módulo que permite asociar a elementos del inventario anuncios en Twitter, Facebook e Instagram.
 """,
 
-'author': "My Company",
-'website': "http://www.yourcompany.com",
+'description': """
+    Módulo desarrollado para la asignatura SIE en el que se permite la publicación de anuncios en RRSS.
+""",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Apps',
-    'version': '0.2',
+'author': "SIE Terry 2020",
+'website': "https://github.com/frotunato/salchigramo",
+
+    'category': 'Marketing',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'product'],
